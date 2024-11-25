bb2b9558ee010721917d7cddb9d5098c072a7f69
WanLum <295386487@qq.com>
2024-11-25 13:02:45 +0800

    feat: :sparkles: 添加 swagger 接口文档

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { generateDocument } from './swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  generateDocument(app)
   await app.listen(process.env.PORT ?? 3000);
+
 }
 bootstrap();
