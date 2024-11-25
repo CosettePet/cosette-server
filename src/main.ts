ba33b0dd00838cea408c96113a200a6ddff8fcc9
WanLum <295386487@qq.com>
2024-11-25 13:50:16 +0800

    style: :lipstick: 代码自动格式化配置与挂载

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
 import { generateDocument } from './swagger'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule)
 
+  // 配置 swagger
   generateDocument(app)
-  await app.listen(process.env.PORT ?? 3000);
 
+  await app.listen(process.env.PORT ?? 3000)
 }
-bootstrap();
+bootstrap()
--- src/swagger.ts
+++ src/swagger.ts
@@ -1,16 +1,14 @@
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 export const generateDocument = (app) => {
-
   // 文档的配置项
   const options = new DocumentBuilder()
-  .setTitle('Cosette接口文档')
-  .setDescription('Cosette接口文档包括后台管理系统、PC端、小程序甚至App等所有的接口')
-  .setVersion('1.0')
-  .build();
+    .setTitle('Cosette接口文档')
+    .setDescription('Cosette接口文档包括后台管理系统、PC端、小程序甚至App等所有的接口')
+    .setVersion('1.0')
+    .build()
   // 创建文档
-  const document = SwaggerModule.createDocument(app, options);
+  const document = SwaggerModule.createDocument(app, options)
 
-  SwaggerModule.setup('/api-docs', app, document);
-
-};
+  SwaggerModule.setup('/api-docs', app, document)
+}
