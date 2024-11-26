f4bd65b76807d34aefa51701dbddcc4b9b9a4142
WanLum <295386487@qq.com>
2024-11-26 11:38:52 +0800

    feat: :sparkles: 添加使用过滤器优雅地统一处理响应体

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@nestjs/common'
-
+import { responseMessage } from '@/utils/useResponse'
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!'
+  getHello(): any {
+    return responseMessage({
+      result: 'Hello World',
+    })
   }
 }
---
+++ src/utils/useResponse.ts
@@ -0,0 +1,12 @@
+import dayjs from 'dayjs'
+
+import { RESPONSE_CODE, RESPONSE_MSG } from '@/enums'
+import type { Response } from '@/typings'
+/**
+ * @description: 统一返回体
+ */
+export const responseMessage = <T = any>(
+  data,
+  msg: string = RESPONSE_MSG.SUCCESS,
+  code: number = RESPONSE_CODE.SUCCESS,
+): Response<T> => ({ data, msg, code, timestamp: dayjs().valueOf() })
